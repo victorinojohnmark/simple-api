e2c440815b2c8f45a51fb1eaafff4ec31c5beae3
victorinojohnmark <victorinojohnmark@gmail.com>
2025-04-26 17:49:42 +0800

    Update(index.php): custom error handler to manage errors gracefully

--- index.php
+++ index.php
@@ -21,6 +21,22 @@ autoloadFiles(__DIR__ . '/controllers');
 autoloadFiles(__DIR__ . '/models');
 autoloadFiles(__DIR__ . '/routes');
 
+// Set a custom error handler to manage errors gracefully
+set_error_handler(function ($severity, $message, $file, $line) {
+    if (!(error_reporting() & $severity)) {
+        return;
+    }
+
+    $data = [
+        'message' => $message,
+        'file' => $file,
+        'line' => $line
+    ];
+
+    Response::json($data, 500);
+    exit();
+});
+
 // Run the App
 try {
     App::run();
